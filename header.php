ba0c7910cef2772af7aa29ebececd21e2648f608
locria <angusl2004@gmail.com>
2016-12-02 15:09:46 -0800

    Used local bootstrap css

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
    <meta charset="utf-8" />
    <title>Math Game</title>
    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet" media="screen"/>
+   <link href="bootstrap-3.3.7-dist\css\bootstrap.min.css" rel="stylesheet" media="screen"/>
 </head>
 <body>
     <header class="jumbotron text-center">
